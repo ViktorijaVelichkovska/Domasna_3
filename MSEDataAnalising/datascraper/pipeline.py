6eb7417127c3a6cc0c623562eb30808612ff431d
viktorijavelichkovska <139005471+ViktorijaVelichkovska@users.noreply.github.com>
2025-02-05 19:59:16 +0100

    Update pipeline.py

--- MSEDataAnalising/datascraper/pipeline.py
+++ MSEDataAnalising/datascraper/pipeline.py
@@ -1,31 +1,21 @@
-from datascraper.filters import filter_1, filter_2, filter_3
+from datascraper.filters import fil1, fil2, fil3
 
 
 def process_data(input_data):
-    # data = filter_1(input_data)
-    data = filter_1(input_data)
+    # data = fil1(input_data)
+    data = fil1(input_data)
     print(data)
-    data = filter_2(data)
+    data = fil2(data)
     print(data)
-    data = filter_3(data)
+    data = fil3(data)
     return data
 
 if __name__ == "__main__":
-    url = "https://www.mse.mk/mk/stats/symbolhistory/KMB"  #Input for option 1
-    url_corrected = "https://www.mse.mk/en/stats/current-schedule"  #Input for option 2
-    # data = process_data(url)  #OPTON 1
-    data = process_data(url)    #OPTION 2
+    url = "https://www.mse.mk/mk/stats/symbolhistory/KMB"  
+    url_corrected = "https://www.mse.mk/en/stats/current-schedule" 
+    # data = process_data(url)  
+    data = process_data(url)   
 
-    #TODO
-    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MSEDataAnalising.settings")
-    # execute_from_command_line(sys.argv)
+  
 
-
-    # print(f"Total number of new companies: {len(data)}")
-    # total_data = 0
-    # for company_code, data_per_company in data:
-    #     total_data += len(data_per_company)
-    #     df = pd.DataFrame(data_per_company)
-    #     df.to_csv(f'all_data/{company_code}.csv', index=False)
-    #
-    # print(f"Total number of new data scraped: {total_data} (rows)")
+   
